9bbc70930518584f243f3cc1fdbc8da4b0f7e2d2
normanb98 <107863262+normanb98@users.noreply.github.com>
2022-11-17 15:04:00 +0100

    Update InitWebDriver.java

--- src/main/java/Intellij_Selenium/InitWebDriver.java
+++ src/main/java/Intellij_Selenium/InitWebDriver.java
@@ -11,11 +11,20 @@ public class InitWebDriver {
     public static WebDriver getDriver() {
         return driver;
     }
+    
+    @BeforeAll
+ public static void classInit() {
+ WebDriverManager.chromedriver().setup();
+ }
 
     @BeforeSuite
     public void beforeSuite() {
-        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        page = new CheckoutPage(driver);
+        page.navigate();
+ 
     }
 
     @AfterSuite (alwaysRun = true)
